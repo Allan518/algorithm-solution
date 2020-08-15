b22c2301359113b050a8656b87b33b2b2239ac9a
zhanganlun@gmail.com <Allan123>
2020-08-15 10:29:53 -0400

    small update

--- src/AngryCowSilverL.java
+++ src/AngryCowSilverL.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
-public class AngryCowSilverL
-{
+
+public class AngryCowSilverL {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader("angry.in"));
         PrintWriter pw = new PrintWriter(new BufferedWriter(
@@ -9,25 +9,36 @@ public class AngryCowSilverL
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
+
         int[] locations = new int[n];
-        for(int i = 0; i < n; i++)
+        for (int i = 0; i < n; i++) {
             locations[i] = Integer.parseInt(br.readLine());
+        }
         Arrays.sort(locations);
-        int min = 0,max = 500000000;
-        while(min != max) {
-            int mid = (min+max)/2,used = 0,last = 0;
-            while(last < n) {
-                used++;
-                int curr = last+1;
-                while(curr < n && locations[curr] - locations[last] <= 2*mid)
+
+        int min = 0, max = 500000000;
+
+        while (min != max) {
+            int last = 0;
+            int mid = (min + max) / 2;
+            int cowUsed = 0;
+            while (last < n) {
+                int curr = last + 1;
+                cowUsed ++;
+                while( curr < n && locations[curr] <= locations[last] + 2 * mid ){
                     curr++;
+                }
                 last = curr;
             }
-            if(used <= k)
+            if( cowUsed > k ){
+                min = mid + 1;
+            }
+            else{
                 max = mid;
-            else
-                min = mid+1;
+            }
         }
+
+        System.out.println(min);
         pw.println(min);
         pw.flush();
     }
