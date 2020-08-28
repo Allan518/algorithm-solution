da7452d2efc50d10eddb692a733f9acabc5158db
zhanganlun@gmail.com <Allan123>
2020-08-28 19:40:05 -0400

    initial versio of the solution

---
+++ src/CowPieTreasureL.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class CowPieTreasureL {
+    static int rows;
+    static int cols;
+    static int[][] matrix;
+    static int[][] maxGoldArray;
+    public static void main(String[] args) throws  Exception{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        rows = Integer.parseInt(stringTokenizer.nextToken());
+        cols = Integer.parseInt(stringTokenizer.nextToken());
+
+        matrix = new int[rows + 2 ][cols];
+        maxGoldArray = new int[rows + 2][cols];
+        for( int i = 1; i <= rows ; i ++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            for( int j = 0; j < cols; j++ ){
+                matrix[i][j] = Integer.parseInt(stringTokenizer.nextToken());
+                if ( i-1 > j ){
+                    matrix[i][j] = Integer.MIN_VALUE;
+                }
+            }
+        }
+        System.out.println(getMaxGold(rows, cols -1 ));
+    }
+
+    static int getMaxGold(int i, int j){
+        if(maxGoldArray[i][j] > 0  ){
+            return maxGoldArray[i][j];
+        }
+        int result = 0;
+        if ( i >= 1 && i <= rows ){
+            if( j == 0 ){
+                result = matrix[i][j];
+            }
+            else{
+                result = matrix[i][j] + Math.max(getMaxGold(i, j-1), Math.max(getMaxGold(i-1, j-1), getMaxGold(i+1, j-1)));
+            }
+        }
+        maxGoldArray[i][j] = result;
+        return result;
+    }
+}
+
+
+
+
