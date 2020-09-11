048310a08e319def8029e5923f730beee626e232
zhanganlun@gmail.com <Allan123>
2020-09-10 21:22:11 -0400

    fix the initial condition

--- src/SubsetSumL.java
+++ src/SubsetSumL.java
@@ -13,9 +13,7 @@ public class SubsetSumL {
             System.out.println(0);
         }
         int[][] matrix = new int[n + 1][total + 1];
-        for (int i = 0; i <= n; i++) {
-            matrix[i][0] = 1;
-        }
+        matrix[0][0] = 1;
 
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= total; j++) {
