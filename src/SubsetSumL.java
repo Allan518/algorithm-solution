9384aa6b5f8934b7ccc1f41ccf3a5f57b56697a9
zhanganlun@gmail.com <Allan123>
2020-09-11 09:18:38 -0400

    avoid double output

--- src/SubsetSumL.java
+++ src/SubsetSumL.java
@@ -12,19 +12,21 @@ public class SubsetSumL {
         if ((n % 4) == 1 || (n % 4) == 2) {
             System.out.println(0);
         }
-        int[][] matrix = new int[n + 1][total + 1];
-        matrix[0][0] = 1;
+        else {
+            int[][] matrix = new int[n + 1][total + 1];
+            matrix[0][0] = 1;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= total; j++) {
-                matrix[i][j] = matrix[i - 1][j];
-                if (j >= i) {
-                    matrix[i][j] = matrix[i][j] + matrix[i - 1][j - i];
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= total; j++) {
+                    matrix[i][j] = matrix[i - 1][j];
+                    if (j >= i) {
+                        matrix[i][j] = matrix[i][j] + matrix[i - 1][j - i];
+                    }
                 }
             }
-        }
 
-        System.out.println(matrix[n][total]);
+            System.out.println(matrix[n][total]);
+        }
 
     }
 }
