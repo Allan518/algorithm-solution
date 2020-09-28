4db27a1a62b553269ff8ba5d275ef3ba6b56b2ae
zhanganlun@gmail.com <Allan123>
2020-09-27 20:11:07 -0400

    initial version of the solution

---
+++ src/Race.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+
+public class Race {
+
+    static int K;
+    static int N;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("race.in"));
+//        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        K = Integer.parseInt(stringTokenizer.nextToken());
+        N = Integer.parseInt(stringTokenizer.nextToken());
+        int[] speed = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            speed[i] = Integer.parseInt(bufferedReader.readLine());
+        }
+
+        PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter("race.out")));
+        for (int i = 0; i < N; i++) {
+            printWriter.println(solve(speed[i]));
+        }
+        printWriter.flush();
+    }
+
+    static int solve(int lowSpeed) {
+        int speed = 0;
+        int ldistance = 0;
+        int rdistance = 0;
+
+        for (int timer = 0 ;;) {
+            speed++;
+            if( ldistance + rdistance >= K ){
+                return timer;
+            }
+            else{
+                timer++;
+                ldistance += speed++;
+            }
+            if (speed > lowSpeed) {
+                timer++;
+                rdistance += --speed;
+            }
+        }
+    }
+}
