ef283b6808c3642cde898c45737e9908a70e58c3
zhanganlun@gmail.com <Allan123>
2020-11-18 21:41:11 -0500

    initial version of the solution

--- src/MilkingOrder.java
+++ src/MilkingOrder.java
@@ -5,86 +5,93 @@ import java.util.StringTokenizer;
 //http://usaco.org/index.php?page=viewproblem2&cpid=832
 
 public class MilkingOrder {  //todo
-    static int N = 0;
-    static int M = 0;
-    static int K = 0;
-    static int[] position;
-    static int[] suborder;
-    static int[][] fixedOrder;
-    static HashMap<Integer, Integer> subMap;
-    static HashMap<Integer, Integer> fullMap;
+    static boolean[] usedCow = new boolean[100];
+    static boolean[] usedPos = new boolean[100];
+    static int[] pos = new int[100];
+
+    static int nCows, M, nFixed;
+
+    static int[] ord = new int[100];
+
+    static int[] cFixed = new int[101];
+    static int[] pFixed = new int[101];
 
     public static void main(String[] args) throws Exception {
         String fileName = "milkorder";
         BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName + ".in"));
 
         StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        N = Integer.parseInt(stringTokenizer.nextToken());
+        nCows = Integer.parseInt(stringTokenizer.nextToken());
         M = Integer.parseInt(stringTokenizer.nextToken());
-        K = Integer.parseInt(stringTokenizer.nextToken());
+        nFixed = Integer.parseInt(stringTokenizer.nextToken());
+
         int onePos = -1;
 
-        position = new int[N];
         stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        suborder = new int[M];
-        subMap = new HashMap<>();
         for (int i = 0; i < M; i++) {
-            suborder[i] = Integer.parseInt(stringTokenizer.nextToken());
-            subMap.put(suborder[i], i);
+            ord[i] = Integer.parseInt(stringTokenizer.nextToken());
         }
-        fullMap = new HashMap<>();
-        for (int i = 0; i < K; i++) {
-            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-            int cowID = Integer.parseInt(stringTokenizer.nextToken());
-            int thisPosition = Integer.parseInt(stringTokenizer.nextToken());
-            if( cowID == 1){
-                onePos = thisPosition ;
-            }
-            position[thisPosition - 1] = cowID;
-            fullMap.put(cowID, thisPosition);
-        }
-        if( onePos == -1  ) {
-            for (int i = N - 1; i >= 0; i--) {
-                for (int j = M - 1; j >= 0; j--) {
-                    if (suborder[j] == position[i]) {
-                        for (int k = j - 1; k >= 0; k--) {
-                            for (int m = i--; m >= 0; m--) {
-                                if (position[j] == 0) {
-                                    position[j] = suborder[k];
-                                    break;
-                                }
-                            }
-                        }
-                        break;
-                    }
-                }
-            }
 
-            for (int i = 0; i < N; i++) {
-                if (position[i] == 0) {
-                    onePos = i + 1;
-                    break;
-                }
+        for (int i = 0; i < nFixed; i++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            cFixed[i] = Integer.parseInt(stringTokenizer.nextToken());
+            pFixed[i] = Integer.parseInt(stringTokenizer.nextToken());
+            cFixed[i]--;
+            pFixed[i]--;
+        }
+        nFixed++;
+        for (int i = 0; i < nCows; i++) {
+            cFixed[nFixed - 1] = 0;
+            pFixed[nFixed - 1] = i;
+            if (isValid()) {
+                onePos = i + 1;
+                break;
             }
         }
         System.out.println(onePos);
-
-
         PrintWriter printWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName + ".out")));
         printWriter.println(onePos);
         printWriter.flush();
-
     }
 
-    static boolean isValid( int onePos){
-        boolean result = false;
-        if( position[onePos] == 0 ){
-            position[onePos] = 1;
-            for (int i = 0; i < M ; i++) {
-
-            }
-
+    static boolean isValid() {
+        for (int i = 0; i < nCows; i++) {
+            usedCow[i] = false;
+            usedPos[i] = false;
         }
-        return result;
+        for (int i = 0; i < nFixed; i++) {
+            if (usedCow[cFixed[i]] && pos[cFixed[i]] == pFixed[i]) {
+                continue;
+            }
+            if (usedCow[cFixed[i]]  ) {
+                return false;
+            }
+            if (usedPos[pFixed[i]]) {
+                return false;
+            }
+            usedCow[cFixed[i]] = true;
+            usedPos[pFixed[i]] = true;
+            pos[cFixed[i]] = pFixed[i];
+        }
+        int j = 0;
+        for (int i = 0; i < M; i++) {
+            int cow = ord[i];
+            if (usedCow[cow]) {
+                if (j > pos[cow]) {
+                    return false;
+                }
+                j = pos[cow];
+                continue;
+            }
+            while (usedPos[j]) {
+                j++;
+                if (j == nCows) {
+                    return false;
+                }
+            }
+            usedPos[j] = true;
+            pos[cow] = j;
+        }
+        return true;
     }
 }
