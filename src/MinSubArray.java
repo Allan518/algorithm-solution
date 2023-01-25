786114fe2dcd032d9a47d73cda22f21cd097d38d
Sophia Zhang <atszhang@gmail.com>
2023-01-24 20:17:38 -0500

    initial version of the solution

--- src/MinSubArray.java
+++ src/MinSubArray.java
@@ -1,4 +1,6 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class MinSubArray {
@@ -29,9 +31,47 @@ public class MinSubArray {
         return result;
     }
 
+    public static int[] getShortestSubsequenceL(int[] nums, int k) {
+        int n = nums.length;
+        int[] result = new int[0];
+        int prefixMod = 0;
+
+        ArrayList<Integer>[] modGroups = new ArrayList[k];
+        for (int i = 0; i < k ; i++) {
+            modGroups[i] = new ArrayList<Integer>();
+        }
+
+        int minLength = Integer.MAX_VALUE;
+        int index = 0;
+        for (int num: nums) {
+            // Take modulo twice to avoid negative remainders.
+            int currentMod = ( num % k + k) % k;
+            if ( currentMod == 0 ){
+                result = new int[]{num};
+                break;
+            }
+            prefixMod = (prefixMod + num % k + k) % k;
+            // Add the count of subarrays that have the same remainder as the current
+            // one to cancel out the remainders.
+            modGroups[prefixMod].add(index++);
+            if (modGroups[prefixMod].size() > 1){
+                int leftIndex = modGroups[prefixMod].get(modGroups[prefixMod].size() - 2) ;
+                int rightIndex =  modGroups[prefixMod].get(modGroups[prefixMod].size() - 1);
+                int currentLength = leftIndex - leftIndex + 1;
+                if (currentLength < minLength) {
+                    result = new int[currentLength];
+                    for( int i = leftIndex; i < rightIndex; i ++){
+                        result[ i - leftIndex] = nums[i];
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] arr = {14, 5, 7, -2, 5, 1};
         int k = 19;
-        System.out.println(getShortestSubsequence(arr, k));
+        System.out.println(Arrays.toString(getShortestSubsequenceL(arr, k)));
     }
 }
